9726ebb52df78e3804cc091daa22c7afb0362369
Shawn Axsom <axs221@gmail.com>
2019-10-06 20:17:03 -0400

    Finished base warrior script

--- 127_0_0_1___21025/default/warrior.js
+++ 127_0_0_1___21025/default/warrior.js
@@ -7,7 +7,7 @@ const canExecute = (creep) => {
 const execute = (creep) => {
     if (canExecute(creep)) {
         const goFight = () => {
-            console.log("goFight");
+            // console.log("goFight");
             const target = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
             if(target) {
                 if(creep.attack(target) == ERR_NOT_IN_RANGE) {
@@ -15,24 +15,8 @@ const execute = (creep) => {
                 }
             }
         };
-        
-        const goBuild = () => {
-            // console.log("goBuild");
-            const source = creep.pos.findClosestByRange(FIND_MY_CONSTRUCTION_SITES)
-            if (creep.build(source, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-                creep.moveTo(source);
-            }
-        }
-        
-        if (_.sum(creep.carry) === creep.carryCapacity) {
-            goFight();
-        } else {
-            goHarvest();
-        }   
-        
-        if (Memory.creeps[creep.name].path && Memory.creeps[creep.name].path.length === 0) {
-            Memory.creeps[creep.name].path = null;
-        }
+
+        goFight();
     }
 };
 
